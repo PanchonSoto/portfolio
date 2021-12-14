755da2687ccf4057255c6a15f1ea077f7f978cc1
Pancho <panchon.ues@gmail.com>
2021-12-14 14:30:22 -0700

    -___-

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,21 @@ const menu = document.querySelector(".enlaces-nav");
 const btnMenu = document.querySelector(".hamburguer");
 const btnCancel = document.querySelector(".btn-cancel");
 
+const nombreElement = document.getElementById("nombre");
+const nombreTxt = 'Web Developer';
+let index = 1;
+
+nombreText()
+function nombreText() {
+    nombreElement.innerText = nombreTxt.slice(0,index);
+    index++;
+    if(index > nombreTxt.length) {
+        index = 1;
+    }
+
+    setTimeout(nombreText, 200);
+}
+
 //para que el menu se esconda
 let ubiacionPrincipal = window.pageYOffset;
 window.addEventListener("scroll", ()=>{
